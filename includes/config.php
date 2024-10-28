8d9b687da7e2120a46ce7cb571281a588b00c7b7
elianacuna <83922885+elianacuna@users.noreply.github.com>
2024-10-28 14:16:00 -0600

    database

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la base de datos
-define('DB_SERVER', 'EACUNAP\SQLEXPRESS');
-define('DB_USERNAME', 'sa');
-define('DB_PASSWORD', 'Holaque2');
-define('DB_NAME', 'NominaConsulting');
+define('DB_SERVER', 'Nomina-Consulting.mssql.somee.com');
+define('DB_USERNAME', 'NominaConsulting_SQLLogin_1');
+define('DB_PASSWORD', 'tybk9g8vgc');
+define('DB_NAME', 'Nomina-Consulting');
 
 // Configuración adicional si es necesario
 define('APP_NAME', 'Nomina-Consulting');
